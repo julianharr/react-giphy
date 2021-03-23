4920b3ca9f756235c1e1c1f14be66b566f44778c
Julian Harrington <jrayharrington@gmail.com>
2021-03-23 18:18:29 +1100

    add counter and change colour on click

--- src/index.jsx
+++ src/index.jsx
@@ -17,23 +17,25 @@ class Hello extends Component {
     super(props);
 
     this.state = {
-      clicked: false
+      clicked: false,
+      counter: 0
     };
   }
 
   handleClick = () => {
     // TODO: change the state of clicked to true
     this.setState({
-      clicked: !this.state.clicked
+      clicked: !this.state.clicked,
+      counter: this.state.counter + 1
     });
   }
 
-// Build and return HTML
+  // Build and return HTML
   render() {
     return (
       <div className={this.state.clicked ? 'clicked' : null}
-       onClick={this.handleClick}>
-       Hello {this.props.name}</div>
+      onClick={this.handleClick}>
+       Hello {this.props.name} {this.state.counter}</div>
     );
   }
 }
