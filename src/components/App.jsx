45451a28a3c3a0eaa7a1d524560118833d5e2166
Julian Harrington <jrayharrington@gmail.com>
2021-04-13 17:20:00 +1000

    bind gif to selected-gif

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-// import giphy from 'giphy-api';
+import giphy from 'giphy-api';
 
 import SearchBar from './search_bar';
 import Gif from './gif';
 import GifList from './gif_list';
 
-// const GIPHY_API_KEY = '1KMPHCBIOe3hOjJwCJQX49sRc6cM0oIm';
+const GIPHY_API_KEY = '1KMPHCBIOe3hOjJwCJQX49sRc6cM0oIm';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   constructor(props) {
     super(props);
@@ -16,24 +15,37 @@ class App extends Component {
       gifs: [],
       selectedGifId: "xT9IgDEI1iZyb2wqo8"
     };
+    this.search = this.search.bind(this);
+    this.selectGif = this.selectGif.bind(this);
+  }
+
+  search(query) {
+    const giphEndpoint = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${query}&limit=30`;
+    fetch(giphEndpoint).then(response => response.json()).then((data) => {
+      const gifs = data.data.map(giph => giph.id);
+      this.setState({
+        gifs: gifs
+      });
+    });
+  }
+
+  selectGif(id) {
+    this.setState({
+      selectedGifId: id
+    });
   }
 
   render() {
-    const gifs = [
-      { id: "xT9IgDEI1iZyb2wqo8" },
-      { id: "26mfgkKTsxNEq3VAs" }
-    ];
-
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
             <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={this.state.gifs} />
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
         </div>
       </div>
     );
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
-// eslint-disable-next-line react/prefer-stateless-function
 class SearchBar extends Component {
   handleUpdate = (event) => {
-    console.log(event.target.value);
+    this.props.searchFunction(event.target.value);
   }
+
   render() {
     return (
-      <input type="text" className="form-search form-control"
-      onChange={this.handleUpdate}/>
+      <input
+        type="text"
+        className="form-control form-search"
+        onChange={this.handleUpdate}
+      />
     );
   }
 }
